991aa3d74a8918872ef40fa65586453db099426a
Rustam Sharipov <rustamshar@inbox.ru>
2023-05-23 10:30:34 +0500

    Добавление метода отрисовки виселицы в класс Render.

--- src/Render.java
+++ src/Render.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 public class Render {
     private StringBuilder string;
+
     public void greeting() {
         System.out.println("Приветствую, ты запустил игру ВИСЕЛИЦА,\n" +
                 "чтобы начать игру нажми на 1, если хочешь выйти жми 0!");
@@ -23,6 +24,55 @@ public class Render {
                 .append("Использованные буквы: ").append(enteredLetters).append("\n")
                 .append("Кол-во неудачных попыток: ").append(numberOfFailures).append("\n");
         System.out.println(string);
+        System.out.println(gallowsRendering(numberOfFailures));
     }
 
+    public String gallowsRendering(int numberOfFail) {
+        String gallows = "";
+        switch (numberOfFail) {
+            case 1 -> gallows = """
+                       |----|
+                       |    0
+                       |
+                       |
+                    ___|___""";
+            case 2 -> gallows = """
+                       |----|
+                       |    0
+                       |    |
+                       |
+                    ___|___""";
+            case 3 -> gallows = """
+                       |----|
+                       |    0
+                       |   /|
+                       |
+                    ___|___""";
+            case 4 -> gallows = """
+                       |----|
+                       |    0
+                       |   /|\\
+                       |
+                    ___|___""";
+            case 5 -> gallows = """
+                       |----|
+                       |    0
+                       |   /|\\
+                       |   /
+                    ___|___""";
+            case 6 -> gallows = """
+                       |----|
+                       |    0
+                       |   /|\\
+                       |   / \\
+                    ___|___""";
+            default -> gallows = """
+                       |----|
+                       |
+                       |
+                       |
+                    ___|___""";
+        }
+        return gallows;
+    }
 }
